7e03be58a5272213241ff7ff6a2cd050c04bc838
PiotrekK2001 <97753158+PiotrekK2001@users.noreply.github.com>
2023-01-06 16:37:15 +0100

    jquery init and get value from inputs at calculator

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,1 +1,22 @@
-console.debug("Hello world");
+import * as $ from 'jquery';
+
+$('#calculate-button').click(() => {
+  // eslint-disable-next-line no-console
+  console.debug(`Wysokość kredytu(PLN): ${$('#loan-amount').val()}`);
+
+  // eslint-disable-next-line no-console
+  console.debug(`Okres kredytowania(miesiące): ${$('#loan-period').val()}`);
+
+  // eslint-disable-next-line no-console
+  console.debug(
+    `Wysokość wpłaty własnej(PLN): ${$('#amount-of-own-contribution').val()}`
+  );
+
+  // eslint-disable-next-line no-console
+  console.debug(`Oprocentowanie kredytu: ${$('#loan-interest-rate').val()}`);
+
+  // eslint-disable-next-line no-console
+  console.debug(
+    `Wysokość spłaty częsciowej(PLN): ${$('#partial-payment-amount').val()}`
+  );
+});
